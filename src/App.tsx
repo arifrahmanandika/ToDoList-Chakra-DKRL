29453c2b28c5c4450e18648a371890bc7b16a422
dkrl-makaryo <arifrahmanandika@gmail.com>
2024-06-13 19:22:56 +0700

    zoom class 29

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ import { Container } from "@chakra-ui/react";
 import Title from "./components/title";
 import ListToDo from "./components/listTodo";
 import ToolsToDo from "./components/toolsTodo";
+import { ITodo } from "./interface";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState<number>(0);
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
   return (
     <Container>
       <Title />
-      <ListToDo />
-      <ToolsToDo />
+      <ListToDo
+        todos={todos}
+        setTodos={setTodos}
+        count={count}
+        setCount={setCount}
+      />
+      <ToolsToDo todos={todos} setTodos={setTodos} count={count} />
     </Container>
   );
 }
